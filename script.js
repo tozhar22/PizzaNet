6b142292ac08efd3fc4c6a65a9d72fab388b0bf7
josias ASSIH <josiasassih4@gmail.com>
2023-04-23 12:30:21 +0100

    AjoutDumodeNuit

--- script.js
+++ script.js
@@ -1,4 +1,41 @@
 
-var div = document.querySelector('.prix');
+var sociale = document.getElementsByClassName("sociale");
+console.log (sociale); 
 
-console.log(div);
+var mode = document.getElementById("modenuit");
+var centre  = document.querySelector("body");
+var nav = document.querySelectorAll("a");
+var cartes = document.getElementsByClassName("carte");
+var soleil = document.getElementById("soleil");
+var navigation = document.getElementsByClassName("nav");
+var semaine = document.getElementById("weekFood");
+var modenuit = false;
+
+mode.addEventListener('click',function(){
+    if (modenuit) {
+        centre.style.backgroundColor="#fff";
+        cartes[0].style.backgroundColor="#fff";
+        cartes[1].style.backgroundColor="#fff";
+        cartes[2].style.backgroundColor="#fff";
+        navigation[0].style.color="#000";
+        navigation[1].style.color="#000";
+        navigation[2].style.color="#000";
+        soleil.setAttribute('src',"assets/claire.jpg");  
+        semaine.style.color="#000";
+        mode.style.backgroundColor="#000";
+        modenuit = false;
+    } else {
+        centre.style.backgroundColor="#0F0F0F";
+        cartes[0].style.backgroundColor="#fff";
+        cartes[1].style.backgroundColor="#fff";
+        cartes[2].style.backgroundColor="#fff";
+        navigation[0].style.color="#fff";
+        navigation[1].style.color="#fff";
+        navigation[2].style.color="#fff";
+        soleil.setAttribute('src',"assets/soleil.png");  
+        semaine.style.color="#fff";
+        mode.style.backgroundColor="#fff";
+        modenuit = true;
+    }
+}, false);
+
